d6b2c8b5e55d53eda96ce2258eff175592e2d2f4
fedoseevtaf <fedoseevtaf@gmail.com>
2022-10-26 20:29:04 +0300

    Add AbstractPlayer to make player more static and independent. Doc all stuff.

--- qb_sample.py
+++ qb_sample.py
@@ -1,67 +1,82 @@
-from typing import Iterable
+'''\
+qb_sample provides tools for sounds's mapping.
+'''
+
+
+from typing import Callable, Iterable
 
 
 class Sample():
+    '''\
+    Sample provides managing for list of sounds
+    without any information about what sound is.
+    '''
 
     def __init__(self, sounds: list, *args,
                  tact_l: int = 3, tact_n: int = 4) -> None:
+
         '''\
         'tact_l' - length of tact
         'tact_n' - number of tacts in sample
 
-        Sample get lists of sounds and them titles, manage
-        that list without any information about what sound is.
-
-        'mapping' is used to remind what time (beat)
+        'mapping' is used to remind what beat
         what sound should be played.
         '''
-
-        self.actual_beat_num = 0
 
         self.tact_l = tact_l
         self.tact_n = tact_n
 
+        self._sounds = sounds
+        self._mapping = list()
+
+        self._actual_beat_num = 0
         self._sample_len = tact_l * tact_n
         self._sounds_len = len(sounds)
 
-        self.sounds = sounds
-        self.mapping = list()
 
     def switch(self, sound_index: int, beat_index: int) -> None:
         '''\
-        The 'switch' should be used to turn on/off
-        defined sound at defined tact.
+        Turn on/off defined sound at defined beat.
         '''
         if 0 <= sound_index < self._sounds_len and 0 <= beat_index < self._sample_len:
-            self.mapping[sound_index][beat_index] ^= 1
+            self._mapping[sound_index][beat_index] ^= 1
 
     def beat(self) -> Iterable:
         '''\
-        Yield all sounds that should play at that beat.
+        Yield all sounds that should be played
+        at that beat for playing them.
         '''
 
-        for sound_index, sound in enumerate(self.sounds):
-            if self.mapping[sound_index][self.actual_beat_num]:
+        for sound_index, sound in enumerate(self._sounds):
+            if self._mapping[sound_index][self.actual_beat_num]:
                 yield sound
 
-        self.actual_beat_num += 1
-        self.actual_beat_num %= self._sample_len
+        self._actual_beat_num += 1
+        self._actual_beat_num %= self._sample_len
 
-    def reset(self) -> None:
-        self.actual_beat_num = 0
+    def goto_start(self) -> None:
+        '''\
+        Turn back play pointer to the beginning of the sample.
+        '''
+
+        self._actual_beat_num = 0
 
     def clear(self) -> None:
-        self.mapping.clear()
-        self.mapping.extend(bytearray(self._sample_len) for _ in range(self._sounds_len))
+        '''\
+        Refill the mapping.
+        '''
+
+        self._mapping.clear()
+        self._mapping.extend(bytearray(self._sample_len) for _ in range(self._sounds_len))
 
     def resize(self, tact_l: int, tact_n: int = None) -> None:
         '''\
-        'resize' method shoud be used if you need to change:
+        Change:
             <*> The time signature. But only meter, because
                 sample don't save note length.
             <*> The 'track' size (amount of tacts in the sample)
 
-        With resizing all the sound sequence will be cleaned
+        With resizing the mapping will be cleaned
         by the 'clear' method.
         '''
 
@@ -72,12 +87,113 @@ class Sample():
         self.clear()
 
     def append(self, sound: object) -> None:
-        self.sounds.append(sound)
-        self.mapping.append(bytearray(self._sample_len))
+        '''\
+        Add new sound and make mapping for it.
+        '''
+
+        self._sounds.append(sound)
+        self._mapping.append(bytearray(self._sample_len))
         self._sounds_len += 1
 
     def remove(self, sound_index: int) -> None:
+        '''\
+        Remove the sound and his mapping by the index of the sound.
+        '''
+
         if 0 <= sound_index < self._sounds_len:
-            del self.sounds[sound_index]
-            del self.mapping[sound_index]
+            del self._sounds[sound_index]
+            del self._mapping[sound_index]
             self._sounds_len -= 1
+
+
+class AbstarctPlayer():
+    '''\
+    Implement non-tech stuff.
+
+    Word about displaying policy:
+    The player has no tools for displaying the sound mapping
+    and providing ui. So for that reason the player use
+    '_draw_sound_callback' to "notify" the ui that sound is
+    successfully added to the sample.
+    '''
+
+    def __init__(self, *args, time_sign: tuple[int] = (4, 8),
+                 bpm: int = 90, tact_n: int = 3) -> None:
+        '''\
+        The 'time signature' is a musician term, read about it on wiki.
+        https://en.wikipedia.org/wiki/Time_signature
+        '''
+
+        self.time_sign = time_sign
+        self.bpm = bpm
+
+        self._sounds = list()
+        self._sample = Sample(self._sounds, tact_l=self.time_sign[0], tact_n=tact_n)
+        self._sample.clear()  # Super important line, clear fill the mapping of the sample
+        self._draw_sound_callback = print
+
+    def add_sound(self, sound, draw_spec=None) -> None:
+        '''\
+        Append sound to the sample and
+        use the '_draw_sound_callback'
+        to display the new sound.
+        '''
+
+        self._sample.append(sound)
+        self._draw_sound_callback(draw_spec)
+    def set_draw_sound_callback(self, callback: Callable) -> None:
+        '''\
+        Read at the class's docs about the displaying policy.
+        '''
+
+        self._draw_sound_callback = callback
+
+    def resize(self, time_sign: tuple[int] = (4, 8), tact_n: int = 3) -> None:
+        '''\
+        Facade for the sample.resize, but using the time signature.
+        '''
+
+        self.time_sign = time_sign
+        self._sample.resize(tact_l=self.time_sign[0], tact_n=tact_n)
+
+    def switch(self, sound_index: int, beat_index: int) -> None:
+        '''\
+        Facade for the Sample.
+        '''
+
+        self._sample.switch(sound_index, beat_index)
+
+    def goto_start(self) -> None:
+        '''\
+        Facade for the Sample.
+        '''
+
+        self._sample.goto_start()
+
+    def clear(self) -> None:
+        '''\
+        Facade for the Sample.
+        '''
+
+        self._sample.clear()
+
+    def rem_sound(self, sound_index: int) -> None:
+        '''\
+        Facade for the Sample.
+        '''
+
+        self._sample.remove(sound_index)
+
+    def get_tact_n(self) -> int:
+        '''\
+        Facade for the Sample.
+        '''
+
+        return self._sample.tact_n
+
+    def get_tact_l(self) -> int:
+        '''\
+        Facade for the Sample.
+        '''
+
+        return self._sample.tact_l
